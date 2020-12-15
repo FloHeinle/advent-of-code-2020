3621289cf3644c4a112d37680b922304e73f80cb
joinez <matthias@hinterschwepfinger.net>
2020-12-15 20:10:44 +0100

    Day 13 part 1

--- lib/day13/day13_m.rb
+++ lib/day13/day13_m.rb
@@ -4,7 +4,21 @@ class Day13M
   FILE = 'lib/day13/test_input'
 
   class << self
-    def part1(file: FILE); end
+    def part1(file: FILE) # rubocop:disable Metrics/AbcSize
+      departure = lines(file).first.to_i
+      schedule = (lines(file).last.split(',') - ['x']).map(&:to_i)
+
+      @buses_departing = {}
+
+      schedule.each do |bus|
+        @buses_departing[bus] = departure - (departure % bus) + bus
+      end
+
+      earliest_bus = @buses_departing.min_by { |_k, v| v }[0]
+      waiting_time = @buses_departing[earliest_bus] - departure
+
+      waiting_time * earliest_bus
+    end
 
     def part2(file: FILE); end
 
--- spec/lib/day13/day13_m_spec.rb
+++ spec/lib/day13/day13_m_spec.rb
@@ -5,7 +5,7 @@ require_relative '../../../lib/day13/day13_m'
 RSpec.describe Day13M do
   context 'when Ruby' do
     it 'Part 1: 295' do
-      skip('Please implement Part 1')
+      expect(described_class.part1).to eq(295)
     end
 
     it 'Part 2: INSERT_EXPECTATION_PART_2' do
